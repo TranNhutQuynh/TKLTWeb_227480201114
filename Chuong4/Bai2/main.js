b46e077f5edc23f77a8411694374d7090bb2984a
Tran Quynh <tranquynh6202@gmail.com>
2025-03-02 12:14:26 +0700

    up bài 2 chương 4

---
+++ Chuong4/Bai2/main.js
@@ -0,0 +1,6 @@
+document.getElementById("btn").addEventListener("click", function () {
+  document.getElementById("text").value = "hello";
+});
+document.getElementById("reset").addEventListener("click", function () {
+  document.getElementById("text").value = "";
+});
