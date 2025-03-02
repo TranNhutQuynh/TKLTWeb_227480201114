27c6426c18f9ef39c379ac8cf460b7d9bd17c0d4
Tran Quynh <tranquynh6202@gmail.com>
2025-03-02 11:29:09 +0700

    up bài 1 chương 4

---
+++ Chuong4/Bai1/main.js
@@ -0,0 +1,7 @@
+function HienThi() {
+    var ten = document.getElementById('name').value;
+    document.getElementById('result_name').innerHTML =  ten;
+
+    var lop = document.getElementById('class').value;
+    document.getElementById('result_class').innerHTML = lop;
+}
