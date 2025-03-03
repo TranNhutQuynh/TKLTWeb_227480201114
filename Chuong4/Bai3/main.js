d13ad035c536cc7eb2335ac51763bb937baf82df
Tran Quynh <tranquynh6202@gmail.com>
2025-03-03 08:18:19 +0700

    up bài 3 chương 4

---
+++ Chuong4/Bai3/main.js
@@ -0,0 +1,7 @@
+document.getElementById('welcome').addEventListener('click', function () {
+    alert('Welcome to student!');
+})
+
+document.getElementById('hello').addEventListener('click', function () {
+    alert('Hello Everybody, I am studying web design with Javscript');
+})
