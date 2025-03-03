efc74fa702b351daa34bcd42a5daf24672ea1807
Tran Quynh <tranquynh6202@gmail.com>
2025-03-03 09:46:16 +0700

    up bài 4

---
+++ Chuong4/Bai4/main.js
@@ -0,0 +1,10 @@
+function cthoten() {
+    var ten = document.getElementById('txtTen').value;
+
+    document.getElementById('ttTen').innerHTML = ten;
+}
+function ctdonvi() {
+    var dv = document.getElementById('txtDonVi').value;
+
+    document.getElementById('ttDV').innerHTML = dv;
+}
