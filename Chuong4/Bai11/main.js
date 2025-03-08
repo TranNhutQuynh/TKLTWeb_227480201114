5ec393f0630944791a12083280063fc9db1ba5b4
Tran Quynh <tranquynh6202@gmail.com>
2025-03-08 10:04:53 +0700

    up Bài 11 chương 4

---
+++ Chuong4/Bai11/main.js
@@ -0,0 +1,12 @@
+function change_blue() {
+  document.body.style.backgroundColor = "blue";
+}
+function change_green() {
+  document.body.style.backgroundColor = "green";
+}
+function change_yellow() {
+  document.body.style.backgroundColor = "yellow";
+}
+function reset() {
+  document.body.style.backgroundColor = "white";
+}
